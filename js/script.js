e417126b313ee6249a944682dc289a44deaaaea6
AlessandroRomanelli <aleromrod@gmail.com>
2016-12-30 16:27:54 +0100

    Added more quotes and tag category

--- js/script.js
+++ js/script.js
@@ -6,48 +6,80 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 var quotes = [
     {quote: "War is a series of disasters which result in a winner.",
     source: "Georges Clemenceau",
-    year: 1921},
+    year: 1921,
+    tag: "war"},
 
     {quote: "Stay out of the road, if you want to grow old.",
     source: "Pink Floyd",
     citation: "Sheep",
-    year: 1977},
+    year: 1977,
+    tag: "music"},
 
     {quote: "It's not that I'm afraid to die, I just don't want to be there when it happens.",
     source: "Woody Allen",
     citation: "Without Feathers",
-    year: 1975},
+    year: 1975,
+    tag: "death"},
 
     {quote: "Once the game is over, the king and the pawn go back in the same box.",
-    source: "Popular Italian proverb"},
+    source: "Popular Italian proverb",
+    tag: "proverb"},
 
     {quote: "Mankind must put an end to war, or war will put an end to mankind.",
     source: "John Fitzgerald Kennedy",
     citation: "Address before the United Nations",
-    year: 1961},
+    year: 1961,
+    tag: "war"},
+
+    {quote: "There are no secrets to success. It is the result of preparation, hard work, and learning from failure.",
+    source: "Colin Powell",
+    tag: "work"}
 
     {quote: "War does not determine who is right — only who is left.",
     source: "Anonymous",
     citation: "first appeared in The Montreal Star",
-    year: 1932},
+    year: 1932,
+    tag: "war"},
+
+    {quote: "One good thing about music, when it hits you, you feel no pain.",
+    source: "Bob Marley",
+    tag: "music"},
 
     {quote: "Sometime they'll give a war and nobody will come.",
     source: "Carl Sandburg",
     citation: "The People",
-    year: 1936},
+    year: 1936,
+    tag: "war"},
+
+    {quote: "Coming together is a beginning; keeping together is progress; working together is success.",
+    source: "Henry Ford",
+    tag: "work"},
 
     {quote: "I believe in the old and sound rule that an ounce of sweat will save a gallon of blood.",
     source: "George S. Patton",
     citation: "Speech to the Third Army",
-    year: 1944},
+    year: 1944,
+    tag: "war"},
+
+    {quote: "There is only one happiness in this life, to love and be loved.",
+    source: "George Sand",
+    tag: "love"},
 
     {quote: "The quickest way to end a war is to lose it.",
-    source: "George Orwell"},
+    source: "George Orwell",
+    tag: "war"},
+
+    {quote: "The consciousness of loving and being loved brings warmth and richness to life that nothing else can bring.",
+    source: "Alvin Redman",
+    citation: "The Epigrams of Oscar Wilde",
+    year: 1952,
+    tag: "love"}
 
     {quote: "I hate war as only a soldier who has lived it can, only as one who has seen its brutality, its stupidity.",
     source: "Dwight D. Eisenhower",
     citation: "Speech in Ottawa",
-    year: 1946},
+    year: 1946,
+    tag: "war"},
 ];
 
 
